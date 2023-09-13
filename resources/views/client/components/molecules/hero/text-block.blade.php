4d90d116ca51107a0bc947dc0fd017ba66809f6a
AdibaHaque13 <adibahaque13@gmail.com>
2023-09-13 21:34:57 +0600

    first commit

---
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -0,0 +1,8 @@
+@push('css')
+    <link rel="stylesheet" href="{{ asset('client/components/molecules/hero/text-block.css') }}">
+@endpush
+
+<div class="text-center text-block-hero py-5">
+    <h1>Stylish Living, Sustainable Choices</h1>
+    <p>Step into the world of online shopping like never before with our stylish e-commerce website. We bring you the latest in fashion and lifestyle products, curated with a keen eye for style and quality. Explore a wide range of clothing, accessories, and lifestyle items that effortlessly blend fashion-forward trends with timeless elegance. Our user-friendly interface ensures a seamless shopping experience, allowing you to browse, select, and purchase your favorite items with ease. From luxurious apparel that exudes sophistication to accessories that add the perfect finishing touch to any outfit, our website is your ultimate destination for all things stylish. Embrace a shopping experience that combines convenience with a passion for fashion, and discover the art of style through our carefully selected collection.</p>
+</div>
